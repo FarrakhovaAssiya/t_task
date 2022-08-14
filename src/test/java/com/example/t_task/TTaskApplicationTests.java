c3a51ce50bb96235d76efe34de580cd3640a2b68
Assiya <farrakhova.02@bk.ru>
2022-08-14 19:06:39 +0600

    Initial commit

---
+++ src/test/java/com/example/t_task/TTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.t_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
